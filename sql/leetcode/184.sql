f192e63569090b8a06b10930787bd2ace55db2a2
diogo-vieira-cabral <diogo.vieira.cabral@gmail.com>
2026-01-28 17:43:25 +0000

    feat: solve leetcode 184
    Filtering Window Functions via CTE

---
+++ sql/leetcode/184.sql
@@ -0,0 +1,34 @@
+-- PROBLEM: 184. Department Highest Salary 
+
+-- BUSINESS CONTEXT: Elite Tier Identification / High-Yield Asset Ranking. 
+
+-- TECHNICAL CHALLENGE: Filtering Window Function outputs via CTE.
+
+```sql
+WITH RankedSalary AS (
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary, -- Forgot a comma
+DENSE_RANK() OVER (
+    PARTITION BY d.id
+    ORDER BY e.salary DESC
+    ) AS rnk 
+FROM 
+    Employee AS e 
+JOIN
+    Department as d ON e.departmentId = d.id
+)
+
+SELECT
+Department,
+Employee,
+Salary
+FROM
+    RankedSalary
+WHERE
+    rnk = 1;
+
+-- need a CTE because cannot filter by a window Function in the same SELECT statement that created it. 
+The ´where´ tries to filter the data before dense rank has been calculated
+```
